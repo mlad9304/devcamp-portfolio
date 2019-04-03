594077a059a58ccd7131600b771daf41f8d38d9a
rndev80 <rndev80@gmail.com>
2019-04-03 16:11:59 +0200

    Updated file upload forms and refacotred functionality into view

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,8 +2,7 @@ class Portfolio < ApplicationRecord
     has_many :technologies
     accepts_nested_attributes_for :technologies, reject_if: ->(attributes){ attributes['name'].blank? }, allow_destroy: true
 
-    include Placeholder
-    validates_presence_of :title, :body, :main_image, :thumb_image
+    validates_presence_of :title, :body
 
     mount_uploader :thumb_image, PortfolioUploader
     mount_uploader :main_image, PortfolioUploader
@@ -15,10 +14,4 @@ class Portfolio < ApplicationRecord
     scope :by_position, -> { order 'position ASC' }
     scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on rails') }
 
-    after_initialize :set_defaults
-  
-    def set_defaults
-        self.main_image ||= Placeholder.image_generator(height: 600, width: 400)
-        self.thumb_image ||= Placeholder.image_generator(height: 350, width: 200)
-    end
 end
